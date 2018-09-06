dd9145a9455281b635988aef62c18f989a4c8dd8
alexoooo <alex@ostrovsky.biz>
2018-09-05 22:01:18 -0400

    removed redundant ajax

--- kzen-launcher-js/src/main/kotlin/tech/kzen/launcher/client/Main.kt
+++ kzen-launcher-js/src/main/kotlin/tech/kzen/launcher/client/Main.kt
@@ -2,8 +2,6 @@ package tech.kzen.launcher.client
 
 import react.dom.render
 import tech.kzen.launcher.client.api.async
-import tech.kzen.launcher.client.api.clientRestApi
-import tech.kzen.launcher.client.api.shellRestApi
 import tech.kzen.launcher.client.components.ProjectLauncher
 import kotlin.browser.document
 import kotlin.browser.window
@@ -16,16 +14,6 @@ fun main(args: Array<String>) {
 
     window.onload = {
         async {
-
-            val artifacts = clientRestApi.listArtifacts()
-            console.log("$$ artifacts: $artifacts")
-
-            val projects = clientRestApi.listProjects()
-            console.log("$$ projects: $projects")
-
-            val running = shellRestApi.runningProjects()
-            console.log("$$ running: $running")
-
             val rootElement = document.getElementById("root")
                     ?: throw IllegalStateException("'root' element not found")
 
