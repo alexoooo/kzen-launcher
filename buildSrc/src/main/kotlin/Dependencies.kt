bb863864d3dc97974d0bd41333b838d44964d72c
alexoooo <alex@ostrovsky.biz>
2021-01-13 12:21:15 -0500

    release 0.20.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,18 +1,14 @@
-const val kotlinVersion = "1.4.20"
+const val kotlinVersion = "1.4.21"
 
 //const val springBootVersion = "2.2.6.RELEASE"
-const val springBootVersion = "2.4.0"
+const val springBootVersion = "2.4.1"
 
 const val dependencyManagementVersion = "1.0.9.RELEASE"
 
 const val coroutinesVersion = "1.4.2"
 //const val serializationVersion = "0.20.0"
 
-//const val wrapperKotlinVersion = "pre.103-kotlin-1.3.72"
-const val wrapperKotlinVersion = "pre.129-kotlin-1.4.20"
-
-//const val kzenLibVersion = "0.13.7"
-//const val kzenAutoVersion = "0.13.0"
+const val wrapperKotlinVersion = "pre.134-kotlin-1.4.21"
 
 const val immutaleJsVersion = "3.8.2"
 const val dexxVersion = "0.7"
@@ -28,12 +24,12 @@ const val kotlinStyledVersion = "5.2.0-$wrapperKotlinVersion"
 const val kotlinExtensionsVersion = "1.0.1-$wrapperKotlinVersion"
 const val kotlinCssVersion = "1.0.0-$wrapperKotlinVersion"
 
-const val guavaVersion = "30.0-jre"
+const val guavaVersion = "30.1-jre"
 //const val seleniumVersion = "3.141.59"
 const val commonsCompressVersion = "1.20"
 
-const val materialUiCoreVersion = "4.11.0"
+const val materialUiCoreVersion = "4.11.2"
 const val materialUiIconsVersion = "4.9.1"
 const val reactSelectVersion = "3.1.1"
-const val jacksonModuleKotlin = "2.11.2"
-const val jacksonDataformatYaml = "2.11.2"
+const val jacksonModuleKotlin = "2.12.1"
+const val jacksonDataformatYaml = "2.12.1"
